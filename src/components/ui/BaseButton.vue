c3391ad806c2d1a5f06dfe85a5d50459928f1c0a
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-22 20:22:27 +0200

    add base badge styling

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -28,8 +28,8 @@ a {
     text-decoration: none;
     padding: 0.75rem 1.5rem;
     font: inherit;
-    background-color: #3a0061;
-    border: 1px solid #3a0061;
+    background-color: #560061;
+    border: 1px solid #560061;
     color: white;
     cursor: pointer;
     border-radius: 30px;
@@ -41,20 +41,20 @@ a:hover,
 a:active,
 button:hover,
 button:active {
-    background-color: #270041;
-    border-color: #270041;
+    background-color: #400041;
+    border-color: #400041;
 }
 
 .flat {
     background-color: transparent;
-    color: #3a0061;
+    color: #560061;
     border: none;
 }
 
 .outline {
     background-color: transparent;
-    border-color: #270041;
-    color: #270041;
+    border-color: #400041;
+    color: #400041;
 }
 
 .flat:hover,
--- src/main.js
+++ src/main.js
@@ -3,18 +3,17 @@ import { createApp } from "vue";
 import router from "./router";
 import store from "./store/index";
 import App from "./App.vue";
-import BaseCard from './components/ui/BaseCard.vue';
-// import BaseButton from './components/ui/BaseButton.vue';
-// import BaseCart from './components/ui/BaseCart.vue';
-
-
+import BaseCard from "./components/ui/BaseCard.vue";
+import BaseButton from "./components/ui/BaseButton.vue";
+import BaseBadge from "./components/ui/BaseBadge.vue";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
 
-app.component('base-card',BaseCard);
-// app.component('base-card',BaseButton);
+app.component("base-card", BaseCard);
+app.component("base-button", BaseButton);
+app.component("base-badge", BaseBadge);
 
 app.mount("#app");
