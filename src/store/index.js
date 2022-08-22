af0fc198163d9316453a963845f154629eb664ab
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-22 22:59:11 +0200

    add files in request vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
 import coachModule from "./modules/coaches/index";
+import requestModule from "./modules/coaches/index";
 
 const store = createStore({
   modules: {
     coaches: coachModule,
+    requests: requestModule,
   },
   state() {
     return {
