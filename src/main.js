e5dbda5d4782ddd1687fcc3e27c41554cfce46e2
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-22 12:17:37 +0200

    add coaches list in index and return it in getters

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 
 import router from "./router";
+import store from "./store/index";
 import App from "./App.vue";
-
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
+
 app.mount("#app");
--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -1,1 +1,8 @@
-export default {};
+export default {
+  coaches(state) {
+    return state.coaches;
+  },
+  hasCoaches(state) {
+    return state.coaches && state.coaches.length > 0;
+  },
+};
