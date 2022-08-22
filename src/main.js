e7a58b24a8f91d6004a3afd966904d211f9ba382
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-22 16:57:36 +0200

    add base-card styling

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import { createApp } from "vue";
 import router from "./router";
 import store from "./store/index";
 import App from "./App.vue";
+import BaseCard from './components/ui/BaseCard.vue';
+// import BaseCart from './components/ui/BaseCart.vue';
+// import BaseCart from './components/ui/BaseCart.vue';
+
+
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card',BaseCard)
 
 app.mount("#app");
--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -3,22 +3,25 @@
         FILTER
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
-        </ul>
-        <h3 v-else>No coaches found.</h3>
+        <base-card>
+            <div class="controls">
+                <button>Refresh</button>
+                <router-link to="/register">Register as Coach</router-link>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id"
+                    :first-name="coach.firstName" :last-name="coach.lastName" :rate="coach.hourlyRate"
+                    :areas="coach.areas"></coach-item>
+            </ul>
+            <h3 v-else>No coaches found.</h3>
+        </base-card>
     </section>
 </template>
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue'
 
 export default {
-    components: {CoachItem},
+    components: { CoachItem },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
