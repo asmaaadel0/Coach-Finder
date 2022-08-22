75664f7236b703d903fa196dfb0212514cf91154
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-22 14:13:18 +0200

    create CoachItem.vue and add styling to it

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li>
+        <h3> {{ fullName }}</h3>
+        <h4>${{ rate }}</h4>
+        <div>
+            <span v-for="area in areas" :key="area">{{ area }}</span>
+        </div>
+        <div class="actions">
+            <router-link to="/coaches/c1/contact">Contact</router-link>
+            <router-link to="/coaches/c1">View Details</router-link>
+        </div>
+    </li>
+</template>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
