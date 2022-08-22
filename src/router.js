de981ecedfa4070fe28d25b34b02bbba7f0e7add
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-22 20:45:04 +0200

    update coach details

--- src/router.js
+++ src/router.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import CoachDetails from './pages/coaches/CoachDetails.vue';
-import CoachList from './pages/coaches/CoachList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsRecieved from './pages/requests/RequestsRecieved.vue';
-import NotFound from './pages/NotFound.vue';
-
-
+import CoachDetails from "./pages/coaches/CoachDetails.vue";
+import CoachList from "./pages/coaches/CoachList.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestsRecieved from "./pages/requests/RequestsRecieved.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -17,6 +15,7 @@ const router = createRouter({
     {
       path: "/coaches/:id",
       component: CoachDetails,
+      props: true,
       children: [{ path: "contact", component: ContactCoach }], //  /coaches/c1/contact
     },
     { path: "/register", component: CoachRegistration },
