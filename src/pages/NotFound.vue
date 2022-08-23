462d6137b711ef69b361676a403180d05d272014
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-23 22:36:32 +0200

    Update NotFound.vue

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>This page could not be d=found - maybe check out all our <router-link to="/coaches">coaches
+                </router-link>
+            </p>
+        </base-card>
+    </section>
+</template>
