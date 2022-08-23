9626c3bcf310089c0151bd945d364a4abc300c65
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-23 21:57:14 +0200

    fetch requests message

--- src/pages/requests/RequestsRecieved.vue
+++ src/pages/requests/RequestsRecieved.vue
@@ -1,10 +1,14 @@
 <template>
+    <base-dialog :show="!!error" title="An error accurred!" @close="handleError">
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <base-card>
             <header>
                 <h2>Requests Recieved</h2>
             </header>
-            <ul v-if="hasRequests">
+            <base-spinner v-if="isLoading"></base-spinner>
+            <ul v-else-if="hasRequests">
                 <requests-item v-for="req in recievedRequests" :key="req.id" :email="req.userEmail"
                     :message="req.message"></requests-item>
             </ul>
@@ -18,12 +22,35 @@ export default {
     components: {
         RequestsItem,
     },
+    data() {
+        return {
+            isLoading: false,
+            error: null,
+        };
+    },
     computed: {
         recievedRequests() {
             return this.$store.getters['requests/requests'];
         },
         hasRequests() {
             return this.$store.getters['requests/hasRequests'];
+        },
+    },
+    created() {
+        this.loadRequests();
+    },
+    methods: {
+        async loadRequests() {
+            this.isLoading = true;
+            try {
+                await this.$store.dispatch('requests/fetchRequests');
+            } catch (error) {
+                this.error = error.message || 'Something went wrong';
+            }
+            this.isLoading = false;
+        },
+        handleError() {
+            this.error = null;
         }
     }
 }
