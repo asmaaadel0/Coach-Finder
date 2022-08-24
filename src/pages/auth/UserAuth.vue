7f8478998c7c616edde27d283e77928de5af3e43
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-24 22:45:26 +0200

    add login

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -66,14 +66,16 @@ export default {
                 return;
             }
             this.isLoading = true;
+
+            const actionPayload = {
+                email: this.email,
+                password: this.password,
+            }
             try {
                 if (this.mode === 'login') {
-                    // ...
+                    await this.$store.dispatch('login', actionPayload);
                 } else {
-                    await this.$store.dispatch('signup', {
-                        email: this.email,
-                        password: this.password,
-                    });
+                    await this.$store.dispatch('signup', actionPayload);
                 }
             }
             catch (error) {
