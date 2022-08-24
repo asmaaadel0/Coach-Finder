f64353a824e997c696a4dbaaddb209b54316cceb
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-24 21:36:16 +0200

    update store auth

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,30 @@
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyANLszfxfbBN9BovIg-tjosFc1ykRRGc0k",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || "Failed to authenticate."
+      );
+      throw error;
+    }
+    console.log(responseData);
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
