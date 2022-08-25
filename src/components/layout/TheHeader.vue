27fd5aeb8e25a720c77f0ce1757ee341dfbb98dc
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-25 10:33:42 +0200

    add login buttons

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -8,13 +8,27 @@
                 <li>
                     <router-link to="/coaches">All Coaches</router-link>
                 </li>
-                <li>
+                <li v-if="isLoggedIn">
                     <router-link to="/requests">Requests</router-link>
+                </li>
+                <li v-else>
+                    <router-link to="/auth">Login</router-link>
                 </li>
             </ul>
         </nav>
     </header>
 </template>
+
+<script>
+export default {
+    computed: {
+        isLoggedIn() {
+            return this.$store.getters.isAuthenticated;
+        },
+    }
+}
+</script>
+
 <style scoped>
 header {
     width: 100%;
--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -10,7 +10,9 @@
             <base-card>
                 <div class="controls">
                     <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-                    <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
+                    <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+                    <base-button v-if="isLoggedIn && !isCoach && !isLoading" link to="/register">Register as Coach
+                    </base-button>
                 </div>
                 <div v-if="isLoading">
                     <base-spinner></base-spinner>
@@ -47,6 +49,9 @@ export default {
         };
     },
     computed: {
+        isLoggedIn() {
+            return this.$store.getters.isAuthenticated;
+        },
         isCoach() {
             return this.$store.getters['coaches/isCoach'];
         },
