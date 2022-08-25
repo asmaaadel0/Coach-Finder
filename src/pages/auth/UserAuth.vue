834b8994a9894d92e7b8105e76715539cc9459a5
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-25 13:12:13 +0200

    add Authentication and routing

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -16,9 +16,7 @@
           <label for="password">Password</label>
           <input type="password" id="password" v-model.trim="password" />
         </div>
-        <p
-          v-if="!formIsValid"
-        >Please enter a valid email and password (must be at least 6 characters long).</p>
+        <p v-if="!formIsValid">Please enter a valid email and password (must be at least 6 characters long).</p>
         <base-button>{{ submitButtonCaption }}</base-button>
         <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
       </form>
@@ -79,6 +77,8 @@ export default {
         } else {
           await this.$store.dispatch('signup', actionPayload);
         }
+        const redirectUrl = '/' + (this.$route.query.redirect || "coaches");
+        this.$router.replace(redirectUrl);
       } catch (err) {
         this.error = err.message || 'Failed to authenticate, try later.';
       }
--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import ContactCoach from "./pages/requests/ContactCoach.vue";
 import RequestsRecieved from "./pages/requests/RequestsRecieved.vue";
 import NotFound from "./pages/NotFound.vue";
 import UserAuth from "./pages/auth/UserAuth.vue";
-
+import store from "./store/index";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -19,11 +19,28 @@ const router = createRouter({
       props: true,
       children: [{ path: "contact", component: ContactCoach }], //  /coaches/c1/contact
     },
-    { path: "/register", component: CoachRegistration },
-    { path: "/requests", component: RequestsRecieved },
-    { path: "/auth", component: UserAuth },
+    {
+      path: "/register",
+      component: CoachRegistration,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/requests",
+      component: RequestsRecieved,
+      meta: { requiresAuth: true },
+    },
+    { path: "/auth", component: UserAuth, meta: { requiresUnauth: true } },
     { path: "/:notFound(.*)", component: NotFound },
   ],
+});
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next("/auth");
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next("/coaches");
+  } else {
+    next();
+  }
 });
 
 export default router;
